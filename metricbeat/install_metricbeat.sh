b0ceb7f79de8a4108e532c044fbec865b6a57c77
Daniel Rich <drich@employees.org>
2019-04-08 20:54:30 -0700

    Tweaks for armv7

--- metricbeat/install_metricbeat.sh
+++ metricbeat/install_metricbeat.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash 
+
+set -x
+set -e		# Exit on error
 
 BUILD_ARCH=$1
 BUILD_VERSION=$2
@@ -7,18 +10,24 @@ cd /tmp
 
 echo "Installing metricbeat $BUILD_VERSION for $BUILD_ARCH..."
 UNTARDIR=/tmp
+TAROPTS="zxf"
+TARFILE="/tmp/metricbeat.tgz"
+PKG=no		# no if installing from tar, yes if pkg directory structure
 case ${BUILD_ARCH} in
   amd64)
-    curl -s -o /tmp/metricbeat.tgz https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${BUILD_VERSION}-linux-x86_64.tar.gz
+    curl -sL -o ${TARFILE} https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${BUILD_VERSION}-linux-x86_64.tar.gz
 #    apk add gcompat	# Needed for linux binary on alpine
     ;;
   i386)
-    curl -s -o /tmp/metricbeat.tgz https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${BUILD_VERSION}-linux-x86.tar.gz
+    curl -sL -o ${TARFILE} https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${BUILD_VERSION}-linux-x86.tar.gz
 #    apk add gcompat	# Needed for linux binary on alpine
     ;;
   armv7|armhf)
-    curl -s o /tmp/metricbeat.tgz http://mirror.archlinuxarm.org/armv7h/community/metricbeat-${BUILD_VERSION}-1-armv7h.pkg.tar.xz
-    UNTARDIR=/tmp/metricbeat-${BUILD_ARCH} && mkdir $UNTARDIR
+    TARFILE="/tmp/metricbeat.xz"
+    curl -sL -o ${TARFILE} http://mirror.archlinuxarm.org/armv7h/community/metricbeat-${BUILD_VERSION}-1-armv7h.pkg.tar.xz
+    UNTARDIR=/tmp/metricbeat-${BUILD_VERSION} && mkdir $UNTARDIR
+    TAROPTS="-Jxf"
+    PKG=yes
     ;;
   *)
     echo "Unsupported architecture: ${BUILD_ARCH}!"
@@ -27,7 +36,7 @@ case ${BUILD_ARCH} in
 esac
 
 cd /tmp
-if [ ! -f metricbeat.tgz ]; then
+if [ ! -f ${TARFILE} ]; then
   echo "Unable to find metricbeat tar file!"
   exit 1
 fi
@@ -37,16 +46,23 @@ tarcp() {
 }
 
 # Untar and install metricbeat - use the same layout as the deb package
-tar xf metricbeat.tgz -C $UNTARDIR
+tar ${TAROPTS} ${TARFILE} -C ${UNTARDIR}
+ls -R $UNTARDIR
 cd metricbeat-${BUILD_VERSION}*
-mkdir /etc/metricbeat/ /usr/share/metricbeat/
-cp metricbeat /usr/bin/
-#find kibana -print | cpio -pdm /usr/share/metricbeat/
-tarcp kibana /usr/share/metricbeat/
-cp *.txt /usr/share/metricbeat/
-#find modules.d -print | cpio -pdm /etc/metricbeat/
-tarcp modules.d /etc/metricbeat/
-cp *.yml /etc/metricbeat/
+if [ "$PKG" = "yes" ]; then
+  tarcp . /
+  mkdir -p /usr/share/metricbeat/bin/
+  if [ -f /usr/bin/metricbeat ]; then	# Move binary to the expected place
+    mv /usr/bin/metricbeat /usr/share/metricbeat/bin/
+  fi
+else
+  mkdir -p /etc/metricbeat/ /usr/share/metricbeat/bin/
+  tarcp kibana /usr/share/metricbeat/
+  cp metricbeat /usr/share/metricbeat/bin/metricbeat
+  cp *.txt /usr/share/metricbeat/
+  tarcp modules.d /etc/metricbeat/
+  cp *.yml /etc/metricbeat/
+fi
 
 # Cleanup after install
 /bin/rm -fr metricbeat.tgz metricbeat
--- metricbeat/run.sh
+++ metricbeat/run.sh
@@ -4,15 +4,15 @@ set -e
 CONFIG_PATH=/data/options.json
 
 ELASTICSEARCH_HOST=$(jq --raw-output ".elasticsearch_host" $CONFIG_PATH)
-ELASTICSEARCH_PORT=$(jq --raw-output ".elasticsearch_post" $CONFIG_PATH)
+ELASTICSEARCH_PORT=$(jq --raw-output ".elasticsearch_port" $CONFIG_PATH)
 ELASTICSEARCH_USER=$(jq --raw-output ".elasticsearch_user" $CONFIG_PATH)
 ELASTICSEARCH_PSWD=$(jq --raw-output ".elasticsearch_password" $CONFIG_PATH)
 
-ESARGS=-E output.elasticsearch.hosts=["${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}"]
+ESARGS="-E output.elasticsearch.hosts=[\"${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}\"]"
 if [ -n "$ELASTICSEARCH_USER" ]; then
-  ESARGS=${ESARGS} -E output.elasticsearch.username="${ELASTICSEARCH_USER}"
+  ESARGS="${ESARGS} -E output.elasticsearch.username=\"${ELASTICSEARCH_USER}\""
 fi
 if [ -n "$ELASTICSEARCH_PSWD" ]; then
-  ESARGS=${ESARGS} -E output.elasticsearch.password="${ELASTICSEARCH_PSWD}"
+  ESARGS="${ESARGS} -E output.elasticsearch.password=\"${ELASTICSEARCH_PSWD}\""
 fi
-metricbeat -e ${ESARGS}
+/usr/bin/metricbeat -e ${ESARGS}
